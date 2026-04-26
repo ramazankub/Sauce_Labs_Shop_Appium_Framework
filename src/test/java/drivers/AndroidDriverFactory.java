9080e8a57034d546fa196a5783373038a665232f
Ramazan Kubanov <rmznkubanov@gmail.com>
2026-04-27 04:06:11 +0500

    fixed driver

--- src/test/java/drivers/AndroidDriverFactory.java
+++ src/test/java/drivers/AndroidDriverFactory.java
@@ -4,47 +4,49 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 import java.net.URL;
-import java.time.Duration;
 import java.util.Properties;
 
-
 public class AndroidDriverFactory {
-        public static AndroidDriver createDriver() {
-            try {
-                Properties properties = new Properties();
-                properties.load(AndroidDriverFactory.class.getClassLoader().getResourceAsStream("capabilities/android.properties"));
 
+    public static AndroidDriver createDriver() {
+        try {
+            Properties properties = new Properties();
+            properties.load(
+                    AndroidDriverFactory.class
+                            .getClassLoader()
+                            .getResourceAsStream("capabilities/android.properties")
+            );
 
-                String app = properties.getProperty("app");
+            String app = properties.getProperty("app", "").trim();
 
-                UiAutomator2Options options = new UiAutomator2Options()
-                        .setPlatformName(properties.getProperty("platformName"))
-                        .setAutomationName(properties.getProperty("automationName"))
-                        .setAutoGrantPermissions(
-                                Boolean.parseBoolean(
-                                        properties.getProperty("autoGrantPermissions")))
-                        .setFullReset(
-                                Boolean.parseBoolean(
-                                        properties.getProperty("fullReset")));
+            UiAutomator2Options options = new UiAutomator2Options()
+                    .setPlatformName(properties.getProperty("platformName"))
+                    .setAutomationName(properties.getProperty("automationName"))
+                    .setAutoGrantPermissions(
+                            Boolean.parseBoolean(
+                                    properties.getProperty("autoGrantPermissions")))
+                    .setFullReset(
+                            Boolean.parseBoolean(
+                                    properties.getProperty("fullReset")));
 
-                if (app != null && !app.isBlank()) {
-                    // CI mode: install apk and allow splash/startup activities
-                    options.setApp(app);
-                    options.setAppActivity("com.swaglabsmobileapp.SplashActivity");
-                    options.setAppWaitActivity("com.swaglabsmobileapp.*");
-                    options.setAppWaitDuration(Duration.ofSeconds(90));
-                } else {
-                    options.setAppPackage(properties.getProperty("appPackage"));
-                    options.setAppActivity(properties.getProperty("appActivity"));
-                }
-
-                return new AndroidDriver(
-                        new URL("http://127.0.0.1:4723"),
-                        options
-                );
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+            if (!app.isBlank()) {
+                // CI mode: APK downloaded by pipeline
+                options.setApp(app);
+            } else {
+                // Local mode: use installed app
+                options.setAppPackage(
+                        properties.getProperty("appPackage"));
+                options.setAppActivity(
+                        properties.getProperty("appActivity"));
             }
+
+            return new AndroidDriver(
+                    new URL(properties.getProperty("appium.url")),
+                    options
+            );
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create Android driver", e);
         }
+    }
 }
