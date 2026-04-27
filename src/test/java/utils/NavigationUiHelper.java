f75fc57b65f74e0c76e929250bd0e7fee4661d98
Ramazan Kubanov <rmznkubanov@gmail.com>
2026-04-27 06:37:03 +0500

    fixed driver

--- src/test/java/utils/NavigationUiHelper.java
+++ src/test/java/utils/NavigationUiHelper.java
@@ -23,7 +23,7 @@ public class NavigationUiHelper {
 
     public void checkTextVisible(String text) {
         $x("//*[contains(@text, \"" + text + "\")]")
-                .shouldBe(visible, Duration.ofSeconds(10));
+                .shouldBe(visible, Duration.ofSeconds(3));
     }
 
     public void checkElementVisible(SelenideElement selenideElement) {
